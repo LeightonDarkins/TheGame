46d53e402a4b10b04616791dba9d1b2d1c88873f
Leighton Darkins <ljdarkins@gmail.com>
2018-06-20 12:57:56 +0200

    LD - Adding New Events And Weights:

--- src/GamePhases.js
+++ src/GamePhases.js
@@ -8,20 +8,36 @@ module.exports = {
     },
     events: [
       {
+        description: `it's a nice, easy drive`,
+        batteryEffect: 10
+      },
+      {
+        description: `your engine starts making an unhealthy sound`,
+        batteryEffect: -5
+      },
+      {
         description: `you burn your tongue with your coffee`,
         batteryEffect: -10
       },
       {
-        description: `it's a nice, easy drive...`,
-        batteryEffect: 5
-      },
-      {
         description: `free donuts at the bakery!`,
-        batteryEffect: 10
+        batteryEffect: 15
       },
       {
-        description: `your engine is making an unhealthy sound...`,
-        batteryEffect: -5
+        description: `traffic on the autobahn, oh no!`,
+        batteryEffect: -20
+      },
+      {
+        description: `you catch all the green lights!`,
+        batteryEffect: 25
+      },
+      {
+        description: `you get stopped by the police!`,
+        batteryEffect: -25
+      },
+      {
+        description: `you get a police escort!`,
+        batteryEffect: 30
       }
     ]
   },
@@ -33,18 +49,37 @@ module.exports = {
       batteryEffect: 0
     },
     events: [
-      { description: `you consolidate some SSCCs`, batteryEffect: 5 },
+      {
+        description: `you consolidate some SSCCs to use fewer transport goods`,
+        batteryEffect: 10
+      },
+      {
+        description: `some fresh fish gets added to your tour at the last minute`,
+        batteryEffect: -5
+      },
       {
         description: `you scan SSCCs instead of tapping them`,
-        batteryEffect: 10
+        batteryEffect: 15
       },
       {
         description: `you lie about the temperature of the frozen goods`,
         batteryEffect: -10
       },
       {
-        description: `some fresh fish gets added to your tour at the last minute`,
-        batteryEffect: -5
+        description: `the delivery papers have already been printed`,
+        batteryEffect: 25
+      },
+      {
+        description: `the pickers laugh at you as you load your truck`,
+        batteryEffect: -20
+      },
+      {
+        description: `all of your picklists are ready to load as soon as you arrive`,
+        batteryEffect: 30
+      },
+      {
+        description: `you accidentally loaded the wrong tour!`,
+        batteryEffect: -25
       }
     ]
   },
@@ -57,12 +92,36 @@ module.exports = {
     },
     events: [
       {
-        description: `the customer hates technology, you must use paper documents`,
-        batteryEffect: -15
+        description: `the customer is happy to see you`,
+        batteryEffect: 10
+      },
+      {
+        description: `you break a jar of the customer's pickles`,
+        batteryEffect: -5
+      },
+      {
+        description: `a pfand sack explodes, bottles are all over the place!`,
+        batteryEffect: -10
       },
       {
         description: `the customer lets you try some of their wares`,
         batteryEffect: 15
+      },
+      {
+        description: `the customer compliments you on your awesome app!`,
+        batteryEffect: 25
+      },
+      {
+        description: `the customer wants to return some rancid meat`,
+        batteryEffect: -20
+      },
+      {
+        description: `the customer has no empties to return`,
+        batteryEffect: 30
+      },
+      {
+        description: `the customer hates technology, you must use paper documents`,
+        batteryEffect: -25
       }
     ]
   },
@@ -75,8 +134,12 @@ module.exports = {
     },
     events: [
       {
-        description: `your device just won't connect to the WLAN`,
-        batteryEffect: -20
+        description: `5`,
+        batteryEffect: 10
+      },
+      {
+        description: `you lost count of your empties `,
+        batteryEffect: -5
       },
       {
         description: `the empties employee is nowhere to be found`,
@@ -84,11 +147,23 @@ module.exports = {
       },
       {
         description: `you have exactly the right number of transport goods`,
-        batteryEffect: 10
+        batteryEffect: 15
+      },
+      {
+        description: `your device just won't connect to the WLAN`,
+        batteryEffect: -20
       },
       {
         description: `your transport manager gives you a hi-5`,
-        batteryEffect: 20
+        batteryEffect: 25
+      },
+      {
+        description: `-25`,
+        batteryEffect: -25
+      },
+      {
+        description: `25`,
+        batteryEffect: 30
       }
     ]
   },
@@ -100,8 +175,38 @@ module.exports = {
       batteryEffect: 0
     },
     events: [
-      { description: `it's a nice, easy drive...`, batteryEffect: 10 },
-      { description: `traffic on the autobahn, oh no!`, batteryEffect: -10 }
+      {
+        description: `it's a nice, easy drive`,
+        batteryEffect: 10
+      },
+      {
+        description: `traffic on the autobahn, oh no!`,
+        batteryEffect: -5
+      },
+      {
+        description: `football hooligans are blocking the road!`,
+        batteryEffect: -10
+      },
+      {
+        description: `you get a free fuel voucher!`,
+        batteryEffect: 15
+      },
+      {
+        description: `a row of ducks are crossing the street`,
+        batteryEffect: -20
+      },
+      {
+        description: `you catch every green light!`,
+        batteryEffect: 25
+      },
+      {
+        description: `you get stopped by the police!`,
+        batteryEffect: -25
+      },
+      {
+        description: `you get a police escort!`,
+        batteryEffect: 30
+      }
     ]
   }
 }
